8eaa0f60860f9c78b7bf1fdc3c3ef534e3a2e1d7
Kseniya <kerzhnerkb@gmail.com>
2023-08-16 14:05:00 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	return (
+		parseFloat(
+			n
+				.toString()
+				.split('')
+				.reverse()
+				.join('')
+		)
+	)
 }
+
+
+// Шаг 1: Преобразовать число в строку - .toString()
+// Шаг 2: Разделить строку на массив - .split('')
+// Шаг 3: Перевернуть массив - .reverse()
+// Шаг 4: Соедините его обратно в строку - .join('')
+// Шаг 5: Превратить входное значение в число с плавающей запятой parseFloat()
